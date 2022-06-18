a776228070e4a968a3c2cd50330b3e6270a987aa
giorgoskef98 <giorgos.kefalas98@gmail.com>
2022-06-18 11:29:20 +0300

    Creating and Organizing a Template dir and its' content for the libraries of the sensors

---
+++ lib/Template/src/template.cpp
@@ -0,0 +1,18 @@
+/* 
+    Arduino 37 Sensor Kit Libraries
+
+    Sensor    --> &name
+    Datasheet --> %link
+
+    Discription of the Sensor
+
+    Created June 2022
+    by George Kefalas - hosted on https://github.com/giorgoskef98
+
+*/
+
+
+#include "template.h"
+
+
+
---
+++ lib/Template/src/template.h
@@ -0,0 +1,24 @@
+/* 
+    Arduino 37 Sensor Kit Libraries
+
+    Sensor    --> &name
+    Datasheet --> %link
+
+    Discription of the Sensor
+
+    Created June 2022
+    by George Kefalas - hosted on https://github.com/giorgoskef98
+
+*/
+
+
+#ifndef Template_Lib_h
+#define Template_Lib_h
+
+#include "Arduino.h"
+
+
+
+
+
+#endif
